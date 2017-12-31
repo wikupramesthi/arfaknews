97e2f3e188ce6742df0ecaa921be5f23d374dfa0
Novis <bibir1983@gmail.com>
2018-01-01 00:53:47 +0700

    update SEO management -> Done

--- application/modules/seo/controllers/seo.php
+++ application/modules/seo/controllers/seo.php
@@ -7,14 +7,14 @@ class Seo extends MX_Controller {
         parent::__construct();
 		$this->load->model('all_model');
     }
-	
+
 	public function index()
 	{
 		if ($this->session->userdata('status_admn') == 'superadmin' || $this->session->userdata('status_admn') == 'admin'){
 			$data['header'] 	= modules::run('global/header/atas');
 			$data['footer'] 	= modules::run('global/footer/bawah');
 			$data['content'] 	= modules::run('seo/seo_view');
-		
+
 			$this->load->view('template',$data);
 		} else {
 			redirect('login');
@@ -23,9 +23,70 @@ class Seo extends MX_Controller {
 
 	public function seo_view()
 	{
-		$this->load->view('seo');
+		if ($this->session->userdata('status_admn') == 'superadmin' || $this->session->userdata('status_admn') == 'admin'){
+			$data['content'] 	= modules::run('seo/edit_data',$kode);
+			$this->load->view('template',$data);
+		} else {
+			redirect('login');
+		}
+		// $this->load->view('seo');
 	}
 
+	public function edit($kode=0)
+	{
+		if ($this->session->userdata('status_admn') == 'superadmin' || $this->session->userdata('status_admn') == 'admin'){
+			$data['header'] 	= modules::run('global/header/atas');
+			$data['footer'] 	= modules::run('global/footer/bawah');
+			$data['content'] 	= modules::run('seo/edit_data',$kode);
+
+			$this->load->view('template',$data);
+		} else {
+			redirect('login');
+		}
+	}
+
+	public function edit_data($kode=0)
+	{
+		$data["d"] 			= $this->db->query('SELECT * FROM tbl_seo where status="active" ')->result();
+		$this->load->view('seo',$data);
+	}
+
+	public function insert()
+	{
+		if ($this->session->userdata('status_admn') == 'superadmin' || $this->session->userdata('status_admn') == 'admin'){
+			$data['header'] 	= modules::run('global/header/atas');
+			$data['footer'] 	= modules::run('global/footer/bawah');
+			$data['content'] 	= modules::run('seo/insert_data');
+
+			$this->load->view('template',$data);
+		} else {
+			redirect('login');
+		}
+	}
+
+
+	public function create()
+	{
+		// echo '<pre>';print_r($this->input->post());die;
+		if ($this->session->userdata('status_admn') == 'superadmin' || $this->session->userdata('status_admn') == 'admin'){
+
+			// $this->all_model->delete_data("tbl_seo","id",$this->input->post('id_seo'));
+			$this->all_model->update_data('tbl_seo','id',$this->input->post('id_seo'),array('status'=>'inactive'));
+
+			$data = array(
+				'id' 			=> '',
+				'title' 		=> $this->input->post('title'),
+				'keyword' 		=> $this->input->post('keyword'),
+				'desc' 			=> $this->input->post('desc'),
+				'status' 		=> 'active'
+			);
+			$this->session->set_flashdata('sks', 'SEO Setting Success');
+			$this->all_model->create_data('tbl_seo',$data);
+			echo "<meta http-equiv='refresh' content='0; url=".base_url()."seo'>";
+		} else {
+			redirect('login');
+		}
+	}
 
 }
 
--- application/modules/seo/views/seo.php
+++ application/modules/seo/views/seo.php
@@ -7,42 +7,46 @@
         <div class="row">
            <div class="col-lg-12">
 
-      <? if ($this->session->flashdata('sks')){ ?>
+      <?php
+       if ($this->session->flashdata('sks')){ ?>
 	<div class="alert alert-success"><?=$this->session->flashdata('sks')?></div>
-	<? } ?>
-	<? if ($this->session->flashdata('msg')){ ?>
+	<?php } ?>
+	<?php if ($this->session->flashdata('msg')){ ?>
 	<div class="alert alert-danger"><?=$this->session->flashdata('msg')?></div>
-	<? } ?>
+	<?php } ?>
 	<div class="bs-callout bs-callout-danger">
-	   <form method="post" role="form" action="<?=base_url()?>login/change_password_process" enctype="multipart/form-data" class="form-horizontal">
+	   <form method="post" role="form" action="<?=base_url().$this->uri->segment(1);?>/create" enctype="multipart/form-data" class="form-horizontal"
+	    onsubmit="return confirm('Do you really want to submit the form?');">
+		<input type="hidden" name="id_seo" value="<?=$d[0]->id;?>">
 		<div class="form-group">
 			<label class="col-sm-2 control-label">Title</label>
 			<div class="col-sm-10">
-				<input type="text" name="title" value="" class="form-control" required>
+				<input type="text" name="title" value="<?=$d[0]->title;?>" class="form-control" required>
 			</div>
 		</div>
 		<div class="form-group">
 			<label class="col-sm-2 control-label">Kayword</label>
 			<div class="col-sm-10">
-				<input type="text" name="keyword" value="" class="form-control" required >
+				<input type="text" name="keyword" value="<?=$d[0]->keyword;?>" class="form-control" required >
 			</div>
 		</div>
 		<div class="form-group">
 			<label class="col-sm-2 control-label">Deskripsi</label>
 			<div class="col-sm-10">
-				<input type="text" name="description" value="" class="form-control" required >
+				<input type="text" name="desc" value="<?=$d[0]->desc;?>" class="form-control" required >
 			</div>
 		</div>
 		<div class="form-group">
 			<div class="col-sm-offset-2 col-sm-10">
 				<div class="row">
 					<div class="col-xs-12 col-md-3">
-						<button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-floppy-disk"></span> Simpan</button>
+						<button type="submit" class="btn btn-success btn-block">
+						<span class="glyphicon glyphicon-floppy-disk"></span> Simpan</button>
 					</div>
 				</div>
 			</div>
 		</div>
-        
+
         </form>
       </div>
     </div>
